b1a7f44e16b5b93bbd2e6aa056c61f5ba151c894
marzieh <marziehmonshizade@yahoo.com>
2023-09-20 18:59:35 +0330

    create header component

---
+++ restaurantMenu/src/components/header/header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <header
+        className="d-flex justify-content-center"
+        style={{ backgroundColor: "#F3EBE1" }}
+      >
+        <img
+          src="https://lanima.menusaz.com/Media_folder/1532/img_1532_1677230630.png"
+          alt="logo"
+          style={{ height: "100px", width: "100px" }}
+          className="p-3"
+        />
+      </header>
+    </>
+  );
+};
+
+export default Header;
