2a5c7f392d4e2f2f9cac9d0ef6b6df29f979253c
marzieh <marziehmonshizade@yahoo.com>
2023-09-25 17:48:08 +0330

    create contacts page

---
+++ src/pages/Contacts/Contacts.tsx
@@ -0,0 +1,63 @@
+const Contacts: React.FC = () => {
+  return (
+    <div className="text-center">
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>شنبه</span>
+        <span className="me-5">9 - 24</span>
+      </div>
+
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>یکشنبه</span>
+        <span>9 - 24</span>
+      </div>
+
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>دوشنبه</span>
+        <span>12 - 24</span>
+      </div>
+
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>سه شنبه</span>
+        <span>12 - 24</span>
+      </div>
+
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>چهارشنبه</span>
+        <span>12 - 24</span>
+      </div>
+
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>پنجشنبه</span>
+        <span>7 - 24</span>
+      </div>
+
+      <div
+        className="d-flex justify-content-between p-2 px-3 rounded-2 my-2"
+        style={{ backgroundColor: "#202020" }}
+      >
+        <span>جمعه</span>
+        <span>7 - 24</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
