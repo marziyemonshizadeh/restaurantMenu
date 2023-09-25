433866b2eed7f658387cd60e2264b5cfb2bd1d87
marzieh <marziehmonshizade@yahoo.com>
2023-09-25 17:45:29 +0330

    add outlet and menu button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { Outlet } from "react-router";
 import "./App.css";
-import AboutUs from "./components/aboutUs/aboutUs";
 import Headers from "./components/header/header";
 import Navbar from "./components/navbar/navbar";
 
-function App(): any {
+const App: React.FC = () => {
   return (
     <>
       <Headers />
       <Navbar />
 
-      <div className="container">
-        <AboutUs />
+      <div className="container text-light d-flex justify-content-center">
+        <Outlet />
+      </div>
+      <div className="d-flex justify-content-center">
+        <button type="button" className="btn btn-warning fw-bold px-3 m-3">
+          Menu
+        </button>
       </div>
     </>
   );
-}
+};
 
 export default App;
