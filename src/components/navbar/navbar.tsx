edd5873a2ca5901707b20911113377db979f1557
marzieh <marziehmonshizade@yahoo.com>
2023-09-25 17:48:42 +0330

    refactor navbar component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,31 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
+// interface Navbar{
+//   onClick: ()=> void;
+// }
 const Navbar: React.FC = () => {
   return (
     <>
       <div className="navbar d-flex justify-content-center">
-        <div className="p-2 border-bottom border-warning border-3">
-          اطلاعات تماس
-        </div>
-        <div className="p-2 border-bottom border-warning border-3">
-          ساعات کاری
-        </div>
-        <div className="p-2 border-bottom border-warning border-3">
+        <Link
+          to="/"
+          className="p-2 border-bottom border-warning border-3 text-light"
+        >
           درباره ی ما
-        </div>
+        </Link>
+        <Link
+          to="/"
+          className="p-2 border-bottom border-warning border-3 text-light"
+        >
+          ساعات کاری
+        </Link>
+        {/* <div className="p-2 border-bottom border-warning border-3"></div> */}
+        <Link
+          to="/"
+          className="p-2 border-bottom border-warning border-3 text-light"
+        >
+          اطلاعات تماس
+        </Link>
       </div>
     </>
   );
